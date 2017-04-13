3d7eb8289f86643ac60bf947b96154920f5aff2e
Nat <nat@cmmc.io>
2017-04-13 18:18:10 +0700

    Add eslint.

--- src/sum.js
+++ src/sum.js
@@ -1,9 +1,9 @@
-export default function sum(...figures) {
-    return figures.reduce((total, current) => {
-        return total + current;
-    });
+export default function sum (...figures) {
+  return figures.reduce((total, current) => {
+    return total + current
+  })
 }
 
-console.log(`HELLO ${1+2}`)
-console.log(`HELLO ${1+2}`)
-console.log(`HELLO ${1+2}`)
+console.log(`HELLO ${1 + 2}`)
+console.log(`HELLO ${1 + 2}`)
+console.log(`HELLO ${1 + 2}`)
