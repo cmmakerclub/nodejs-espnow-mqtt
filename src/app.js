740125b6b79888628faf9dff5517002554bf9b5d
Nat <nat@cmmc.io>
2017-05-28 00:41:26 +0700

    push to mqtt.

--- src/app.js
+++ src/app.js
@@ -28,23 +28,35 @@ client.on('message', function (topic, message) {
   console.log('================')
   console.log(message.length)
 
-  // if (checksum(message)) {
-  //   if (message[0] === 0xff && message[1] === 0xfa) {
-  //     let type = message.slice(2, 5)
-  //     let name = message.slice(5, 11)
-  //     let mac = message.slice(27, 27 + 6)
-  //
-  //     console.log(`type = `, type)
-  //     console.log(`name = `, name.toString())
-  //     console.log(`val1 = `, message.readUInt32LE(11))
-  //     console.log(`val2 = `, message.readUInt32LE(15))
-  //     console.log(`val3 = `, message.readUInt32LE(19))
-  //     console.log(`batt = `, message.readUInt32LE(23))
-  //     console.log(`mac = `, mac.toString('hex'))
-  //   } else {
-  //     console.log('invalid header')
-  //   }
-  // } else {
-  //   console.log('invalid checksum')
-  // }
+  if (checksum(message)) {
+    if (message[0] === 0xfc && message[1] === 0xfd) {
+      console.log(message)
+      let mac1 = message.slice(2, 2 + 6)
+      let mac2 = message.slice(2 + 6, 2 + 6 + 6)
+      console.log(mac1, mac2)
+      let len = message[2 + 6 + 6]
+      let payload = message.slice(2 + 6 + 6 + 1, message.length - 1)
+      console.log(`len = ${len}, payload = ${payload.toString('hex')}`)
+      // if (checksum(payload)) {
+      //   console.log('YAY!')
+      // }
+    }
+    //   if (message[0] === 0xff && message[1] === 0xfa) {
+    //     let type = message.slice(2, 5)
+    //     let name = message.slice(5, 11)
+    //     let mac = message.slice(27, 27 + 6)
+    //
+    //     console.log(`type = `, type)
+    //     console.log(`name = `, name.toString())
+    //     console.log(`val1 = `, message.readUInt32LE(11))
+    //     console.log(`val2 = `, message.readUInt32LE(15))
+    //     console.log(`val3 = `, message.readUInt32LE(19))
+    //     console.log(`batt = `, message.readUInt32LE(23))
+    //     console.log(`mac = `, mac.toString('hex'))
+    //   } else {
+    //     console.log('invalid header')
+    //   }
+  } else {
+    console.log('invalid checksum')
+  }
 })
